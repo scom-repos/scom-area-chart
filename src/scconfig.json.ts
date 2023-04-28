0c8f77fe8970753e1d93091bcbc182b2188a1241
Bryan <bryan.dev@instajs.com>
2023-04-28 17:56:34 +0700

    Initial commit

---
+++ src/scconfig.json.ts
@@ -0,0 +1,19 @@
+export default {
+  "name": "@scom-area-chart/main",
+  "version": "0.1.0",
+  "env": "",
+  "moduleDir": "src",
+  "main": "@scom-area-chart/main",
+  "modules": {
+    "@scom-area-chart/main": {
+      "path": "main"
+    },
+    "@scom-area-chart/global": {
+      "path": "global"
+    },
+    "@scom-area-chart/assets": {
+      "path": "assets"
+    }
+  },
+  "dependencies": {}
+}
---
+++ test/modules/module1/index.tsx
@@ -0,0 +1,69 @@
+import { Module, customModule, Container } from '@ijstech/components';
+import ScomAreaChart from '@scom/scom-area-chart';
+
+@customModule
+export default class Module1 extends Module {
+    constructor(parent?: Container, options?: any) {
+        super(parent, options);
+    }
+
+    async init() {
+        super.init();
+    }
+
+    render() {
+        return <i-panel>
+            <i-scom-area-chart
+                margin={{ left: 'auto', right: 'auto' }}
+                data={{
+                    apiEndpoint: 'https://api.dune.com/api/v1/query/2030745/results?api_key=324WhvsCHWCji2pkgtfa0JDqDu8j0FdD',
+                    options: {
+                        title: 'ETH Staked - Cumulative',
+                        options: {
+                            xColumn: {
+                                key: 'date',
+                                type: 'time'
+                            },
+                            yColumns: [
+                                'total_eth',
+                            ],
+                            stacking: true,
+                            groupBy: 'depositor_entity_category',
+                            seriesOptions: [
+                                {
+                                    key: 'CEX',
+                                    color: '#d52828'
+                                },
+                                {
+                                    key: 'Liquid Staking',
+                                    color: '#d2da25'
+                                },
+                                {
+                                    key: 'Others',
+                                    color: '#000000'
+                                },
+                                {
+                                    key: 'Staking Pools',
+                                    color: '#49a34f'
+                                },
+                                {
+                                    key: 'Unidentified',
+                                    color: '#bcb8b8'
+                                }
+                            ],
+                            xAxis: {
+                                title: 'Date',
+                                tickFormat: 'MMM YYYY'
+                            },
+                            yAxis: {
+                                title: 'ETH deposited',
+                                labelFormat: '0,000.00ma',
+                                position: 'left'
+                            }
+                        }
+                    }
+                }}
+            />
+        </i-panel>
+    }
+}
