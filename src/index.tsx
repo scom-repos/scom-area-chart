0876fd4a060fc93dca09542232d1bf4692b8748e
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-08-16 14:47:43 +0800

    Fix onCustomDataChanged error

--- src/index.tsx
+++ src/index.tsx
@@ -157,11 +157,13 @@ export default class ScomAreaChart extends Module {
             const dataSourceSetup = new ScomChartDataSourceSetup(null, {
               ...this._data, 
               chartData: JSON.stringify(this.chartData),
-              onCustomDataChanged: async (data: any) => {
-                onChange(true, {
-                  ...this._data, 
-                  ...data
-                });
+              onCustomDataChanged: async (dataSourceSetupData: any) => {
+                if (onChange) {
+                  onChange(true, {
+                    ...this._data, 
+                    ...dataSourceSetupData
+                  });
+                }
               }
             });
             const hstackBtnConfirm = new HStack(null, {
